a59544616fa51aa38d44c7c49eff77392997ff08
newesish <hugo@tbf.mx>
2014-06-12 16:09:43 -0500

    se agregaron validadciones basicas a todos los modelos, y se hizo la pantalla del perks.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :project
+	validates :user_id, :project_id, :comments, presence:true
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,4 @@
 class Image < ActiveRecord::Base
 	belongs_to :gallery
+	validates :image_url, :galery_id, presence:true
 end
